cf5f580ae50ee1f55893c624c5308b7efd403595
unknown <varshitham209.@gmail.com>
2025-03-23 18:55:15 +0530

    fourth

--- backend.java
+++
@@ -1,5 +0,0 @@
-@Tag("hello-world")
-@JsModule("src/HelloWorld.js")
-public class HelloWorld extends PolymerTemplate<HelloWorldModel> {
-  // --
-}
--- frontend.java
+++
@@ -1,8 +0,0 @@
-getElement().executeJs("return 'adoptedStyleSheets' in document")
-  .then(Boolean.class, supported -> {
-    if (supported) {
-      System.out.println("Feature is supported");
-    } else {
-      System.out.println("Feature is not supported");
-    }
-});
